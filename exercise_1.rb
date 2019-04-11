0624280ae29a83f13d796e1b32f57cb554700519
domw30 <domw30@gmail.com>
2019-04-11 12:54:34 +0100

    exercise 4, add equal class

--- exercise_1.rb
+++ exercise_1.rb
@@ -1,7 +1,129 @@
-def expect_to_be_equal(x, y)
-  'Test passes! :)' if x == true && y == true
-  'Test fails! D:' if x == true && y == false
+# # Exercise 1
+
+# def expect_to_be_equal(x, y)
+#   'Test passes! :)' if x == true && y == true
+#   'Test fails! D:' if x == true && y == false
+# end
+#
+# puts expect_to_be_equal(true, false)
+# puts expect_to_be_equal(true, true)
+#
+
+# Exercise 2
+
+# class Expect
+#
+#   def to_be_equal(x, y)
+#     puts 'Test passes! :)' if x == true && y == true
+#     puts 'Test fails! D:' if x == true && y == false
+#   end
+#
+# end
+#
+# expect = Expect.new
+# puts expect.to_be_equal(true, true)
+# puts expect.to_be_equal(true, false)
+
+
+# Exercise 3
+#
+# class Expect
+#
+#   #attr_reader :true_or_false
+#
+#   def initialize(true_or_false)
+#     @true_or_false = true_or_false
+#   end
+#
+#   def true_or_false
+#     @true_or_false
+#   end
+#
+#   def to_equal(x)
+#     puts "Test passes! :)" if x == true
+#     puts "Test fails! D:" if x == false
+#   end
+#
+# end
+#
+# expect = Expect.new(true)
+# puts expect.to_equal(true)
+# puts expect
+
+
+# Exercise 4
+
+class Expect
+
+  def initialize(value)
+    @value = value
+  end
+
+  def to(comparison)
+    if comparison == true
+      puts "Test passes! :)"
+    else
+      puts "Test fails! D:"
+    end
+  end
+
 end
 
-puts expect_to_be_equal(true, false)
-puts expect_to_be_equal(true, true)
+
+class Equal
+
+  def compare(y)
+    if y == true
+      true
+    else y == false
+      false
+    end
+  end
+
+end
+
+
+expect = Expect.new(true)
+equal = Equal.new
+expect.to(equal.compare(false))
+
+
+# class Expect
+#
+#   def initialize(value)
+#     @value = value
+#   end
+#
+#   def to(comparison)
+#     if comparison.value == @value
+#       puts "Test passes! :)"
+#     else
+#       puts "Test fails! D:"
+#     end
+#   end
+#
+# end
+#
+#
+# class Equal
+#
+#   attr_reader :value
+#
+#   def initialize(value)
+#     @value = value
+#   end
+#
+#   def compare(y)
+#     if y == true
+#       true
+#     else y == false
+#       false
+#     end
+#   end
+#
+# end
+#
+#
+# expect = Expect.new(true)
+# equal = Equal.new(false)
+# expect.to(equal)
--- exercise_1_spec.rb
+++ exercise_1_spec.rb
@@ -3,3 +3,21 @@ expect_to_be_equal(true, true)
 "Test passes! :)"
 expect_to_be_equal(true, false)
 "Test fails! D:"
+
+
+> Expect.new.to_be_equal(true, true)
+=> "Test passes! :)"
+> Expect.new.to_be_equal(true, false)
+=> "Test fails! D:"
+
+
+Expect.new(true).to_equal(true)
+=> "Test passes! :)"
+Expect.new(true).to_equal(false)
+=> "Test fails! D:"
+
+
+> Expect.new(true).to(Equal.new(true))
+=> "Test passes! :)"
+> Expect.new(true).to(Equal.new(false))
+=> "Test fails! D:"
